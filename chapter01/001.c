a64e3c898b749021331d07cd32eee0bf55130c8b
atul.kumar <atul@synsoftcom.onmicrosoft.com>
2025-02-16 16:31:04 +0530

    Add files

---
+++ chapter01/001.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+declaring variables 
+
+*/
+int main(){
+    int emp_num;
+    float salary;
+    char grade;
+    double balance_amt;
+    unsigned short int account_no;
+    unsigned long int transactionNumbers;
+
+
+
+}
